577c6c2093f963b24fbdd4c76e866b68e162514b
Matvey Labutin <1matvey1579@gmail.com>
2025-09-21 19:01:10 +0000

    first commit

---
+++ lab1/main.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+#include "include/lab1.hpp"
+
+int main() {
+    std::string name = "Matvey";
+    str(name);
+    return 0;
+}
---
+++ lab1/source/lab1.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+#include "../include/lab1.hpp"
+
+
+int str(std::string s) {
+    std:: cout << "Hello, " << s << "!\n";
+    return 0;
+}
